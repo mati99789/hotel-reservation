d577636af8f8b86c20ea5366e359e4ecef724052
Mateusz Urbaniak <matteus.urbaniak@hotmail.com>
2025-01-06 14:00:00 +0100

    Feat: Add test non user access.

--- api/booking_handler_test.go
+++ api/booking_handler_test.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"hotelReservetion/api/middleware"
 	"hotelReservetion/db/fixtures"
@@ -10,6 +9,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 	"time"
 )
@@ -18,23 +18,22 @@ func TestAdminGetBookings(t *testing.T) {
 	db := setup()
 	defer db.tearddown(t)
 
-	admin := fixtures.AddUser(db.Store, "admin@o2.pl", "Admin", "Admin", types.AdminRole)
-	hotel := fixtures.AddHotel(db.Store, "Bellucia", "France", 2, nil)
-	room := fixtures.AddRoom(db.Store, types.Normal, false, 300, hotel.ID)
+	var (
+		admin   = fixtures.AddUser(db.Store, "admin@o2.pl", "Admin", "Admin", types.AdminRole)
+		user    = fixtures.AddUser(db.Store, "user@o2.pl", "User 1", "User 1", types.GuestRole)
+		hotel   = fixtures.AddHotel(db.Store, "Bellucia", "France", 2, nil)
+		room    = fixtures.AddRoom(db.Store, types.Normal, false, 300, hotel.ID)
+		booking = fixtures.AddBooking(db.Store, room.ID, admin.ID, 2, time.Now(), time.Now().AddDate(0, 0, 2))
 
-	booking := fixtures.AddBooking(db.Store, room.ID, admin.ID, 2, time.Now(), time.Now().AddDate(0, 0, 2))
-	_ = booking
-
-	app := fiber.New()
-	apiv1 := app.Group("/api/v1", middleware.JWTAuthentications)
-	adminGroup := apiv1.Group("/admin", middleware.AuthorizeRole(types.AdminRole))
+		app        = fiber.New()
+		apiv1      = app.Group("/api/v1", middleware.JWTAuthentications)
+		adminGroup = apiv1.Group("/admin", middleware.AuthorizeRole(types.AdminRole))
+	)
 
 	bookingHandler := NewBookingHandler(db.Store)
 	adminGroup.Get("/users", bookingHandler.HandleGetBookings)
 
 	token := CreateTokenFromUser(admin)
-
-	fmt.Println("token", token)
 
 	req := httptest.NewRequest(http.MethodGet, "/api/v1/admin/users", nil)
 	req.Header.Set("X-API-Token", token)
@@ -55,4 +54,24 @@ func TestAdminGetBookings(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	if len(bookings) != 1 {
+		t.Fatalf("Expected 1 booking, got %d", len(bookings))
+	}
+
+	if reflect.DeepEqual(booking, bookings[0]) {
+		t.Fatalf("Expected %v, got %v", bookings[0], bookings[0])
+	}
+
+	// Test non-admin cannot access the bookings
+	req = httptest.NewRequest(http.MethodGet, "/api/v1/admin/bookings", nil)
+	req.Header.Set("X-API-Token", CreateTokenFromUser(user))
+
+	res, err := app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if res.StatusCode == http.StatusOK {
+		t.Fatalf("Expected status code non %d, got %d. Response body: %s", http.StatusOK, response.StatusCode, res.Body)
+	}
 }
